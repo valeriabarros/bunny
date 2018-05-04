69bac665e22bb2d49ad4f517ee38c1ede4eadcf1
Val <valeriabarros.ifg@gmail.com>
2018-05-04 08:10:38 -0300

    Handling error Updating layout

--- public/index.php
+++ public/index.php
@@ -2,6 +2,7 @@
 use \Psr\Http\Message\ServerRequestInterface as Request;
 use \Psr\Http\Message\ResponseInterface as Response;
 use \GuzzleHttp\Client as Client;
+use GuzzleHttp\Exception\ClientException;
 
 require '../vendor/autoload.php';
 
@@ -11,24 +12,28 @@ $container = $app->getContainer();
 $container['view'] = new \Slim\Views\PhpRenderer('../templates/');
 
 $app->get('/', function (Request $request, Response $response) {
-    $response = $this->view->render($response, 'index.phtml', ['name' => 'GET']);
+    $response = $this->view->render($response, 'index.phtml', ['text' => "Let's try?"]);
     return $response;
 });
 
-$app->post('/send', function (Request $aa, Response $response)  {
+$app->post('/send', function (Request $request, Response $response)  {
     $client = new Client();
     $movieData = $client->request('GET', 'https://tv-v2.api-fetch.website/random/movie')->getBody();
-    $synopsis = json_decode($movieData)->synopsis;                     
-
-
-    echo PHP_EOL;
-    // echo $response->getBody();
-    // echo $response->getHeader('Content-Length');
-
-
-    // $response = $this->view->render($response, 'index.phtml', ['name' => 'POST']);
-    // return $response;
-    echo 'he';
+    $text = json_decode($movieData)->synopsis;                     
+    $formData = array (
+        'title' => 'Valéria Barros - Job Application for Bunny Inc.',
+        'script' => $text,
+        'fake_project' => 1,
+        'talentID' => 47524
+    );
+    $headers = ['Authorization' => 'Basic MTMxMjMxOjU3ZDUwZTRlN2M1YTRmYmQwMDYxZGVjYTY1OGJhZThh','Content-Type' =>'application/x-www-form-urlencoded'];
+    try {
+        $client->request('POST', "https://api.voicebunny.com/projects/addBooking", ['form_params' => $formData, 'headers' => $headers])->getBody();
+    } catch (ClientException $e) {
+        $error = "Something goes wrong with Voice Bunny API.";
+    }
+    $response = $this->view->render($response, 'index.phtml', ['text' => $text, 'error' => $error]);
+    return $response;
 });
 
 $app->run();
